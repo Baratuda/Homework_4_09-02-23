4c0b294bdd1d17fc7a1613cd8755116182a77b32
Egor Baranovskiy <egorpifagop@gmail.com>
2023-02-09 20:09:19 +0300

    First task was done

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿//
+Console.Clear();
+Console.WriteLine("Please enter the number and degree to raise a number to a power");
+Console.WriteLine("-------------------------------------------------------------------");
+int x=0;
+while(x==0){
+   Console.Write("Please enter the number: ");
+   int number = int.Parse(Console.ReadLine());
+   Console.WriteLine("-------------------------------------------------------------------");
+   if(number!=0){
+      Console.Write("Please enter a power: ");
+      int power = int.Parse(Console.ReadLine());
+      Console.WriteLine("------------------------------------------------------------------- ");
+      double result = Math.Pow(number,power);
+      Console.WriteLine($"Result: {result}");
+      Console.WriteLine("------------------------------------------------------------------- ");
+      x=1; 
+   } else Console.WriteLine("Number haven't to equals  0 !!!");
+}
