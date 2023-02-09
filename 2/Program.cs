bd2f1fdfda1dd4a4034daa97ba3071fd6c34db44
Egor Baranovskiy <egorpifagop@gmail.com>
2023-02-09 20:31:31 +0300

    All tasks was done

---
+++ 2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+string stringNumber = null;
+int[] numbersArray;
+int lengthNumber = 0;
+int sumResult = 0;
+int number = 0;
+int x = 0;
+int j = 0;
+    
+//This method output string message
+void outPutStringMessge(string str = null, int x = 0){
+   if(str==null && x==0)
+      Console.WriteLine("-------------------------------------------------------------------");
+   else if(str!=null && x==0){
+      outPutStringMessge();
+      Console.Write(str);
+   }else Console.WriteLine($"{str} {x}");
+}
+//This method calculate sum numbers of number
+void numberSumCalculator(int number){
+   while(x==0){
+      if(number!=0){
+         lengthNumber = (int)Math.Pow(10,stringNumber.Length-1);
+         numbersArray = new int[stringNumber.Length]; 
+         for(int i = lengthNumber; 1<=i; i/=10, j++){
+            numbersArray[j] = number/i;
+            number -= numbersArray[j]*i; 
+            sumResult += numbersArray[j];
+         }
+         outPutStringMessge("Result: ", sumResult);
+         x=1; 
+      } else outPutStringMessge("Number haven't to equals  0 !!!\n");
+   }    
+}
+    
+Console.Clear();
+outPutStringMessge("Please enter the number: ");
+stringNumber = Console.ReadLine();
+number = int.Parse(stringNumber);
+numberSumCalculator(number);
---
+++ 3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] numbersArray = new int[8];
+int[] arrayCopy= new int[8];
+int count = 0;
+string stringNumber = "";
+string message = "";
+ 
+//This method output string message
+void outPutStringMessge(string str = null, int x = 0){
+   if(str==null && x==0)
+      Console.WriteLine("-------------------------------------------------------------------");
+   else if(str!=null && x==0){
+      outPutStringMessge();
+      Console.WriteLine(str);
+   }else Console.WriteLine($"{str} {x}");
+}
+//This method fills an array with numbers.
+void fillArrayNumbers(){
+   outPutStringMessge("Please enter 0 for exit from cycle.");
+   outPutStringMessge("Please enter the numbers: ");
+   while(!stringNumber.Equals("0")){
+      stringNumber = Console.ReadLine();
+      if(count >= numbersArray.Length-1)
+         Array.Resize(ref numbersArray, count+count/2);
+      numbersArray[count] = int.Parse(stringNumber);
+      count++;
+   }
+}
+
+Console.Clear();
+fillArrayNumbers();
+message = "["+String.Join(", ", Array.FindAll(numbersArray, number => number !=0))+"]";
+outPutStringMessge(message);
+
